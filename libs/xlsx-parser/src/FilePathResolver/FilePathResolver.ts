2b8516962ddd4162944ccbe668268cbb89ded917
shabashab <shabashab.04@gmail.com>
2021-08-04 15:16:59 +0300

    refactor(xlsx-parser): remove redundant class
    Remove FilePathResolver and its interface

--- libs/xlsx-parser/src/FilePathResolver/FilePathResolver.ts
+++
@@ -1,8 +0,0 @@
-import IFilePathResolver from "./IFilePathResolver";
-import appRoot from "app-root-path";
-
-export default class FilePathResolver implements IFilePathResolver {
-  resolve(fileName: string): string {
-    return appRoot.resolve(fileName);
-  }
-}
--- libs/xlsx-parser/src/FilePathResolver/IFilePathResolver.ts
+++
@@ -1,3 +0,0 @@
-export default interface IFilePathResolver {
-  resolve(fileName: string): string;
-}
