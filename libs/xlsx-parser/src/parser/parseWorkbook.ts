64eb7f0bd3037fd438f38d7a43963cede2004e70
shabashab <shabashab.04@gmail.com>
2021-08-05 16:07:33 +0300

    refactor(xlsx-model): add return type
    Add return type to parseWorkbook function

--- libs/xlsx-parser/src/parser/parseWorkbook.ts
+++ libs/xlsx-parser/src/parser/parseWorkbook.ts
@@ -1,7 +1,8 @@
 import { Workbook } from "@xlsx-model/excel";
+import { WorkbookModel } from "@xlsx-model/models";
 import WorkbookParserFactory from "../Parsers/WorkbookParserFactory";
 
-export function parseWorkbook(workbook: Workbook) {
+export function parseWorkbook(workbook: Workbook): WorkbookModel {
   const workbookParser = new WorkbookParserFactory().build();
   return workbookParser.parse(workbook);
 }
