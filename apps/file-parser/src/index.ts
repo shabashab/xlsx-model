fd626158227b57efd9f27218cb2a05a2394b8acb
shabashab <shabashab.04@gmail.com>
2021-09-30 12:43:23 +0300

    feat(file-parser): implement the application

--- apps/file-parser/src/index.ts
+++ apps/file-parser/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, world");
+import { XlsxFileModelParser } from "@xlsx-model/xlsx-parser";
+import { WorkbookModelSerializer } from "@xlsx-model/serializer";
+import fs from "fs/promises";
+
+async function main() {
+  const parser = new XlsxFileModelParser("input.xlsx");
+  const serializer = new WorkbookModelSerializer();
+  const workbook = await parser.parse();
+  const result = serializer.serialize(workbook);
+  await fs.writeFile("output.json", JSON.stringify(result));
+}
+
+main();
